1e06a7d7bcf66cfe910df5897037fbc302a49133
Trevor Allen <trevor2492@gmail.com>
2021-08-14 20:21:36 -0500

    added logic to rotate through the colors with 1 bug that I'll fix later

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="App">hello</div>;
+  const colorArray = ["blue", "red", "yellow", "green"];
+
+  const [index, setIndex] = useState(0);
+
+  const handleClick = () => {
+    index === 3 ? setIndex(0) : setIndex(index + 1);
+    console.log("index: " + index);
+  };
+
+  return (
+    <div className="App">
+      <div
+        style={{
+          height: "100px",
+          width: "100px",
+          backgroundColor: colorArray[index],
+        }}
+        onClick={() => handleClick()}
+      />
+      <div
+        style={{
+          height: "100px",
+          width: "100px",
+          backgroundColor: colorArray[index + 1],
+        }}
+        onClick={() => handleClick()}
+      />
+      <div
+        style={{
+          height: "100px",
+          width: "100px",
+          backgroundColor: colorArray[index + 2],
+        }}
+        onClick={() => handleClick()}
+      />
+      <div
+        style={{
+          height: "100px",
+          width: "100px",
+          backgroundColor: colorArray[index + 3],
+        }}
+        onClick={() => handleClick()}
+      />
+    </div>
+  );
 }
 
 export default App;
